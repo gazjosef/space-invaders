b3cfaf9a4b908f762ef3401491338b6668cba268
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-05-31 21:03:23 +1000

    Updated HTML/CSS

---
+++ script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const squares = document.querySelectorAll('.grid div');
+  const resultDisplay = document.querySelector('#result');
+  let width = 15;
+  let currentShooterIndex = 202;
+  let currentInvaderIndex = 0;
+  let alienInvadersTakenDown = [];
+  let result = 0;
+  let direction = 1;
+  let invaderID;
+
+  // Define The Alien Invaders
+  const alienInvaders = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+  ];
+});
