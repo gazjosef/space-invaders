49d834f28dd77a21d732739f0dc4505484ed27be
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-06-01 22:58:26 +1000

    Added Federik Tutorial Part One

---
+++ FrederikDeBleser/start/js/game.js
@@ -0,0 +1,25 @@
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+const GAME_STATE = {
+  playerX: 0,
+  playerY: 0,
+};
+
+function setPosition($el, x, y) {
+  $el.style.transform = `translate(${x}, ${y})`;
+}
+
+function createPlayer($container) {
+  GAME_STATE.playerX = GAME_WIDTH / 2;
+  GAME_STATE.playerY = GAME_HEIGHT - 50;
+  const $player = document.createElement('img');
+  $player.src = 'img/player-blue-1.png';
+  $player.className = 'player';
+  $container.appendChild($player);
+}
+
+function init() {
+  const $container = document.querySelector('.game');
+  createPlayer($container);
+}
