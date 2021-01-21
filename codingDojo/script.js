61162bf1d7ff1cd1dad8ea892fa3cdc904cb65fb
Gareth Hind <garethjhind@gmail.com>
2021-01-21 17:49:32 +1100

    Updated

--- codingDojo/script.js
+++ codingDojo/script.js
@@ -3,6 +3,9 @@ const hero = {
     left: 500
 }
 
+let missiles = []
+
+
 document.onkeydown = (e) => {
     if(e.keyCode === 37) {
         console.log("LEFT");
@@ -14,10 +17,23 @@ document.onkeydown = (e) => {
         moveHero()   
     } else if(e.keyCode === 32) {
         console.log("SPACE");
+        missiles.push({
+            left: hero.left + 15,
+            top: hero.top
+        })
+        console.log(missiles);
+        drawMissiles()
     }
 }
 
 function moveHero() {
     document.getElementById('hero').style.left = hero.left + "px"
 }
-moveHero()
+
+
+function drawMissiles() {
+    document.getElementById('missiles').innerHTML = '';
+    for(let missile = 0; missile < missiles.length; missile++) {
+        document.getElementById('missiles').innerHTML += `<div class='missile' style='left:${missiles[missile].left}px; top:${missiles[missile].top}px;'></div>`
+    }
+}
