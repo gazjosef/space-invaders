d5c2670a80a4d9395fed549d88ba08ad0b1817b4
Gareth Hind <garethjhind@gmail.com>
2021-01-21 17:21:48 +1100

    Updated Coding Dojo

--- codingDojo/script.js
+++ codingDojo/script.js
@@ -1,8 +1,23 @@
+const hero = {
+    top: 700,
+    left: 500
+}
+
 document.onkeydown = (e) => {
-    console.log("click", e);
     if(e.keyCode === 37) {
         console.log("LEFT");
+        hero.left = hero.left - 10;
+        moveHero()
     } else if (e.keyCode === 39) {
         console.log("RIGHT");
+        hero.left = hero.left + 10;
+        moveHero()   
+    } else if(e.keyCode === 32) {
+        console.log("SPACE");
     }
 }
+
+function moveHero() {
+    document.getElementById('hero').style.left = hero.left + "px"
+}
+moveHero()
