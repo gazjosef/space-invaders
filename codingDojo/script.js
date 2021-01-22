3c2817a618c413fbf0759c79197ec3c4f91f1b13
Gareth Hind <garethjhind@gmail.com>
2021-01-22 23:28:22 +1100

    Updated .css & .js

--- codingDojo/script.js
+++ codingDojo/script.js
@@ -37,3 +37,16 @@ function drawMissiles() {
         document.getElementById('missiles').innerHTML += `<div class='missile' style='left:${missiles[missile].left}px; top:${missiles[missile].top}px;'></div>`
     }
 }
+
+function moveMissiles() {
+    for(let missile = 0; missile < missiles.length; missile++) {
+        missiles[missile].top = missiles[missile].top - 5;
+    }
+}
+
+function gameLoop() {
+    setTimeout(gameLoop, 100);
+    moveMissiles()
+    drawMissiles()
+}
+gameLoop()
